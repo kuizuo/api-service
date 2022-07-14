8c34383e536f8bdffb171cfdb1f2444e577728d3
kuizuo <kuizuo12@163.com>
2022-07-14 20:30:39 +0800

    chore: init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+})
