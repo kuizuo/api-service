1a5c1ad017ae0b612c6b6deca5e39808b06f0497
kuizuo <kuizuo12@163.com>
2022-08-08 07:59:23 +0800

    feat: cors

---
+++ server/middleware/cors.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  const { res } = event
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+})
+
