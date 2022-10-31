6dca6d0525ebda05c3b70f5ed9d101f1e2d07c95
kuizuo <kuizuo12@163.com>
2022-10-31 23:40:06 +0800

    feat: api integration

---
+++ server/api/weather.ts
@@ -0,0 +1,19 @@
+import { getCityCode } from '~~/utils/contant'
+import { TimeUnitMap } from '~~/utils/time'
+
+interface Query {
+  city: string
+}
+
+export default defineEventHandler(async (event) => {
+  event.context.cache = { ttl: TimeUnitMap.hour }
+
+  const { city = '' } = useQuery<Query>(event)
+
+  const id = getCityCode(city)
+
+  // http://api.help.bj.cn/
+  const data = await (await fetch(`https://api.help.bj.cn/apis/weather/?id=${id}`)).json()
+
+  return data
+})
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,16 +1,11 @@
 import 'h3'
 
-interface Limit {
-  count: number
-}
-
 interface Cache {
   ttl: number
 }
 
 declare module 'h3' {
   export interface H3EventContext  {
-    limit: Limit
     cache: Cache
   }
 }
