6cefad6c3f818175e21bd0446e4f4627a3de022e
Kuizuo <hi@kuizuo.cn>
2023-09-08 02:18:12 +0800

    chore: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'API-Service',
+      script: './.output/server/index.mjs',
       exec_mode: 'cluster',
       instances: '1',
-      PORT: 8040,
+      port: 8040,
       env: {
+        NODE_ENV: 'production',
         NUXT_PUBLIC_API_URL: 'https://api.kuizuo.cn',
       },
-      script: './.output/server/index.mjs',
     },
   ],
 }
